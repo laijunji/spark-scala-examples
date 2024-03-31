9a1f489cfba443aba93ff5fd85f6a65b381ea827
junji lai <33259190+laijunji@users.noreply.github.com>
2024-03-31 17:24:43 +0800

    fix bugs

--- src/main/scala/com/sparkbyexamples/spark/rdd/CreateEmptyRDD.scala
+++ src/main/scala/com/sparkbyexamples/spark/rdd/CreateEmptyRDD.scala
@@ -16,16 +16,12 @@ object CreateEmptyRDD extends App{
   println(rddString)
   println("Num of Partitions: "+rdd.getNumPartitions)
 
-  rddString.saveAsTextFile("c:/tmp/test5.txt")
-
   val rdd2 = spark.sparkContext.parallelize(Seq.empty[String])
   println(rdd2)
   println("Num of Partitions: "+rdd2.getNumPartitions)
 
-  rdd2.saveAsTextFile("c:/tmp/test3.txt")
 
   // Pair RDD
-
   type dataType = (String,Int)
   var pairRDD = spark.sparkContext.emptyRDD[dataType]
   println(pairRDD)
